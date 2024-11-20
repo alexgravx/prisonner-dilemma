950915323a4569941049073617e2ba968282170d
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-11-21 00:15:39 +0100

    [UPDATE] packages for NextUI

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,13 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+const {nextui} = require("@nextui-org/react");
+
 module.exports = {
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./content/**/*.{md,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: ["class"],
   theme: {
     extend: {},
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '12': '12px',
+    }
   },
-  plugins: [require("@tailwindcss/typography")],
+  darkMode: "class",
+  plugins: [
+    nextui(),
+    require("@tailwindcss/typography")],
 }
