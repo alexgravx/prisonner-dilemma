f52233f6b26c41e7027b234b14a314b28d08bb45
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-11-21 00:15:58 +0100

    [ADD] sliders for parameters

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,19 +1,47 @@
+"use client";
+import * as React from "react";
 import Image from 'next/image'
+import {NextUIProvider} from "@nextui-org/react";
+import {Slider} from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div className="prose dark:prose-invert">
-      <article key={"test"}>
-        <h1>Prisonner Dilemma</h1>
-        <div className="relative p-3 rounded-lg  bg-gray-200 text-black mt-8">
+    <NextUIProvider>
+      <div className="prose dark:prose-invert max-w-4xl">
+        <h1 className="mt-8">
+          Prisonner Dilemma
+        </h1>
+        <div className="relative p-3 rounded-lg bg-gray-200 text-black mt-8">
           <Image
+            className="rounded-lg mt-0 mb-0"
             src="/prisonner.jpg"
             width={1000}
-            height={3000}
+            height={1000}
             alt="Prisonner Dilemma illustration"
           />
         </div>
-      </article>
-    </div>
+        <div>
+          <h1 className="mt-8">
+            Parameters selection
+          </h1>
+          <Slider size="lg" step={1} color="primary" label="Tentation"
+          showSteps={true} maxValue={7} minValue={0} defaultValue={5}
+          className="max-w-md" 
+          />
+          <Slider size="lg" step={1} color="primary" label="Cooperation"
+          showSteps={true} maxValue={7} minValue={0} defaultValue={3}
+          className="max-w-md" 
+          />
+          <Slider size="lg" step={1} color="primary" label="Punishment"
+          showSteps={true} maxValue={7} minValue={0} defaultValue={1}
+          className="max-w-md" 
+          />
+          <Slider size="lg" step={1} color="primary" label="Duperie"
+          showSteps={true} maxValue={7} minValue={0} defaultValue={0}
+          className="max-w-md" 
+          />
+        </div>
+      </div>
+    </NextUIProvider>
   )
 }
