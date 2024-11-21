dd37de59a4eda25e4002a4f39a0b5199b043531b
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-11-22 00:28:28 +0100

    [UPDATE] CORS policy for backend

--- api/main.py
+++ api/main.py
@@ -2,11 +2,27 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from dilemma_pkg import main
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class Strategy(BaseModel):
     strategy_label: str
     population: list[float]
-
-app = FastAPI()
 
 @app.get("/")
 def hello_world():
