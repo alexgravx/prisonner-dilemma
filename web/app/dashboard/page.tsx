094becbb18ddbc5d760e4c02308adf06249adf61
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-11-24 02:12:13 +0100

    [UPDATE] minor fixes and front update

--- web/app/dashboard/page.tsx
+++ web/app/dashboard/page.tsx
@@ -7,6 +7,10 @@ import {ResultLineChart} from "@/components/chart"
 export default function Home() {
   const [turns, setTurns] = React.useState<SliderValue>(100);
   const [pop, setPop] = React.useState<SliderValue>(100);
+  const [T, setT] = React.useState<SliderValue>(5);
+  const [C, setC] = React.useState<SliderValue>(3);
+  const [P, setP] = React.useState<SliderValue>(1);
+  const [D, setD] = React.useState<SliderValue>(0);
 
   return (
     <NextUIProvider>
@@ -17,22 +21,22 @@ export default function Home() {
         <div className="flex flex-row justify-around">
           <div className="flex flex-col w-80">
             <Slider size="lg" step={1} color="primary" label="Tentation"
-            showSteps={true} maxValue={7} minValue={0} defaultValue={5}
-            className="max-w-md" 
+            showSteps={true} maxValue={7} minValue={0} value={T}
+            onChange={setT} className="max-w-md"
             />
             <Slider size="lg" step={1} color="primary" label="Cooperation"
-            showSteps={true} maxValue={7} minValue={0} defaultValue={3}
-            className="max-w-md" 
+            showSteps={true} maxValue={7} minValue={0} value={C}
+            onChange={setC} className="max-w-md"
             />
           </div>
           <div className="flex flex-col w-80">
             <Slider size="lg" step={1} color="primary" label="Punishment"
-            showSteps={true} maxValue={7} minValue={0} defaultValue={1}
-            className="max-w-md" 
+            showSteps={true} maxValue={7} minValue={0} value={P}
+            onChange={setP} className="max-w-md"
             />
             <Slider size="lg" step={1} color="primary" label="Duperie"
-            showSteps={true} maxValue={7} minValue={0} defaultValue={0}
-            className="max-w-md" 
+            showSteps={true} maxValue={7} minValue={0} value={D}
+            onChange={setD} className="max-w-md"
             />
           </div>
         </div>
@@ -41,22 +45,25 @@ export default function Home() {
         </h1>
         <div className="flex flex-row justify-around">
             <Slider size="lg" step={1} color="primary" label="Population"
-            showSteps={false} maxValue={500} minValue={20} value={pop}
+            showSteps={false} maxValue={300} minValue={20} value={pop}
             onChange={setPop} className="max-w-sm"
             />
             <Slider size="lg" step={1} color="primary" label="Nb of rounds"
-            showSteps={false} maxValue={500} minValue={20} value={turns}
+            showSteps={false} maxValue={300} minValue={20} value={turns}
             onChange={setTurns} className="max-w-sm"
             />
-        </div>
-        <div>
-          {turns}
-          {pop}
         </div>
         <h1 className="mt-10">
           Arena Results
         </h1>
-        <ResultLineChart turns={Number(turns)} pop={Number(pop)} />
+        <ResultLineChart
+          turns={Number(turns)}
+          pop={Number(pop)}
+          T={Number(T)}
+          C={Number(C)}
+          P={Number(P)}
+          D={Number(D)}
+        />
       </div>
     </NextUIProvider>
   )
--- web/components/chart.tsx
+++ web/components/chart.tsx
@@ -17,15 +17,19 @@ async function getData(url:URL) {
 
 var colors = ["#111111", "#356000", "#148f77", "#d35400", "#1b4f72", "#85c1e9", "#f4d03f", "#283747", "#d98880", "#148f77", "#e74c3c", "#d2b4de"]
 
-export function ResultLineChart({ turns, pop }: { turns: number; pop: number }) {
+export function ResultLineChart({ turns, pop, T, C, P, D }: { turns: number; pop: number, T: number, C: number, P: number, D: number }) {
   const [chartData, setChartData] = useState([]);
   const [players, setPlayers] = useState<string[]>([]);
 
   useEffect(() => {
-    async function getChartData(turns:number=100, pop:number=100) {
+    async function getChartData(turns:number=100, pop:number=100, T:number=5, C:number=3, P:number=1, D:number=0) {
       const full_url = new URL("http://localhost:8000/arena");
       turns && full_url.searchParams.set('turns', turns.toString())
       pop && full_url.searchParams.set('pop', pop.toString())
+      T && full_url.searchParams.set('T', T.toString())
+      C && full_url.searchParams.set('P', C.toString())
+      P && full_url.searchParams.set('C', P.toString())
+      D && full_url.searchParams.set('D', D.toString())
       let data = await getData(full_url)
       setChartData(data);
     }
@@ -35,9 +39,9 @@ export function ResultLineChart({ turns, pop }: { turns: number; pop: number }) 
       let players = await getData(full_url)
       setPlayers(players);
     }
-    getChartData(turns, pop);
+    getChartData(turns, pop, T, C, P, D);
     getPlayers();
-  }, [turns, pop]);
+  }, [turns, pop, T, C, P, D]);
 
   return (
     <ResponsiveContainer width="100%" height="100%" className="min-h-[40rem]">
