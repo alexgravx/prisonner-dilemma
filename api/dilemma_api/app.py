374a4ce6f689304af71ec3c90b9a41a715aba704
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-12-11 00:34:21 +0100

    [API] update api with .env

--- api/dilemma_api/app.py
+++ api/dilemma_api/app.py
@@ -4,12 +4,16 @@ from dilemma_pkg import main
 
 from fastapi.middleware.cors import CORSMiddleware
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
+    f"http://{os.getenv('FRONTEND_URL')}",
+    f"http://{os.getenv('FRONTEND_URL')}:8080",
+    f"http://{os.getenv('FRONTEND_URL')}:{os.getenv('FRONTEND_PORT')}",
 ]
 
 app.add_middleware(
@@ -38,7 +42,6 @@ def get_ping():
 
 @app.post("/arena")
 def get_arena(data: DataInput) -> list[Strategy]:
-    print(data)
     arena_turns = main.main(data.turns, data.pop, data.T, data.C, data.P, data.D, data.player_list)
     arena_result = [Strategy(root=turn) for turn in arena_turns]
     return arena_result
