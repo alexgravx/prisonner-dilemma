0752f954f6ef4b6e2b6d05fc7c98af064cc87e7c
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-12-12 00:17:40 +0100

    [FIX] CORS and build issue

--- web/components/custom-chart.tsx
+++ web/components/custom-chart.tsx
@@ -49,7 +49,7 @@ export function ArenaChart(
 
   const checkServerStatus = async () => {
     try {
-      const response = await fetch(`http://${process.env.NEXT_PUBLIC_BACKEND_URL}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/ping`);
+      const response = await fetch(`https://${process.env.NEXT_PUBLIC_BACKEND_URL}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/ping`);
       if (response.status === 200) {
         setIsServerUp(true);
       } else {
@@ -71,7 +71,7 @@ export function ArenaChart(
       D:number,
       player_list: string[]
     ) {
-      const url = new URL(`http://${process.env.NEXT_PUBLIC_BACKEND_URL}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/arena`);
+      const url = new URL(`https://${process.env.NEXT_PUBLIC_BACKEND_URL}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/arena`);
       const data_output = await getData(
         url,
         {
